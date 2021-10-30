b92c161d781088aba5f940e0b3b19c2142bf74a3
Rhea <rheachhabra1011@gmail.com>
2021-10-30 14:05:58 +0530

    Add Timber

--- app/src/main/java/com/example/carcinofit/BaseApplication.kt
+++ app/src/main/java/com/example/carcinofit/BaseApplication.kt
@@ -2,8 +2,14 @@ package com.example.carcinofit
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+
+    override fun onCreate() {
+        Timber.plant(Timber.DebugTree())
+        super.onCreate()
+    }
 }
