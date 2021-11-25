fdc193eb515c173710e980f141908d643bc27208
shrey <bhatt.shrey1224@gmail.com>
2021-11-25 23:41:41 +0530

    Debug Keystore

--- app/src/main/java/com/example/carcinofit/BaseApplication.kt
+++ app/src/main/java/com/example/carcinofit/BaseApplication.kt
@@ -1,6 +1,8 @@
 package com.example.carcinofit
 
 import android.app.Application
+import android.util.Log
+import com.example.carcinofit.other.AppSignatureHelper
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -11,5 +13,11 @@ class BaseApplication : Application() {
     override fun onCreate() {
         Timber.plant(Timber.DebugTree())
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            AppSignatureHelper(this).appSignatures.forEach {
+                Timber.d(it)
+            }
+        }
     }
 }
